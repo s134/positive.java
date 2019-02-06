dc2601a071c636148bb5d9a29b778ed6fdc57246
s134 <47376550+s134@users.noreply.github.com>
2019-02-06 14:10:55 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.Scanner;
+ class ideone
+{
+    public static void main(String[] args) 
+    {
+        int number;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        number = scan.nextInt();
+        scan.close();
+        if(number > 0)
+        {
+            System.out.println(" positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println("negative number");
+        }
+        else
+        {
+            System.out.println( "neither positive nor negative");
+        }
+    }
+}
